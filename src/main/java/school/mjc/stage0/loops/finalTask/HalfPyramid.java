582c25014f5c3a57eb45cb373be34101b8f46fe0
mirjambilovj <106926296+mirjambilovj@users.noreply.github.com>
2022-10-16 23:01:22 +0500

    solved3

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,23 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int i,j;
+        int st = 1;
+        int sp = cathetusLength-1;
+        for(i=1; i<=cathetusLength; i++)
+        {
+            for(j=1; j<=sp; j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1; j<=st; j++)
+            {
+                System.out.print("*");
+            }
+            sp--;
+            st++;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int sum=0;
+        for (int i=1; i<=power; i++) {
+            sum = (int) Math.pow(numberToPrint, i);
+        }
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
